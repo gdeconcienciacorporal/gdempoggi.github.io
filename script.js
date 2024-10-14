80d508460897451b6a41b4c04d2f96325fc9c721
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-10-14 12:36:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 12',
-    data: [13, 21, 26], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 2',
+    data: [13, 15, 20], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
